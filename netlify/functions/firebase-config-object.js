7e368febef4aa41fe57ca1ed7fc5f5811007ad4e
Matt Malone <mattagape@me.com>
2024-10-30 05:27:10 -0500

    updates

--- netlify/functions/firebase-config-object.js
+++ netlify/functions/firebase-config-object.js
@@ -1,13 +1,16 @@
 exports.handler = async (event, context) => {
   const firebaseConfig = {
-    apiKey: process.env.API_KEY,        // Considered non-sensitive for public use
+    apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
     projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
   };
 
+  // Encode the config as a Base64 string
+  const encodedConfig = Buffer.from(JSON.stringify(firebaseConfig)).toString("base64");
+
   return {
     statusCode: 200,
-    body: JSON.stringify(firebaseConfig),
+    body: JSON.stringify({ data: encodedConfig }),  // Wrap the encoded data in a JSON object
   };
 };
